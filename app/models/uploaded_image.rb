d9a7945feeb298b865024feb1efd55c1795d5132
d.krasnov <d.krasnov@1tvch.ru>
2015-01-29 19:02:18 +0300

    UploadedImage version method changed to resize_to_limit

--- app/models/uploaded_image.rb
+++ app/models/uploaded_image.rb
@@ -2,7 +2,7 @@ class UploadedImage < Upload
   mount_uploader :file, CkeUploadsImageUploader
 
   def self.image_versions
-    {file: {resize_to_fit: [728, 10000]}}
+    {file: {resize_to_limit: [728, 10000]}}
   end
 
   def self.thumb_versions
