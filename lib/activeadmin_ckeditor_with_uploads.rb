a43de3af0ce7fa909d0e27818f0fcb6cb6f16d56
d.krasnov <d.krasnov@1tvch.ru>
2015-01-29 18:42:53 +0300

    models moved to app/models

--- lib/activeadmin_ckeditor_with_uploads.rb
+++ lib/activeadmin_ckeditor_with_uploads.rb
@@ -6,10 +6,6 @@ require "formtastic/inputs/ckeditor_input"
 require 'activeadmin_ckeditor_with_uploads/uploaders/cke_uploads_file_uploader'
 require 'activeadmin_ckeditor_with_uploads/uploaders/cke_uploads_image_uploader'
 
-require 'activeadmin_ckeditor_with_uploads/models/upload'
-require 'activeadmin_ckeditor_with_uploads/models/uploaded_file'
-require 'activeadmin_ckeditor_with_uploads/models/uploaded_image'
-
 require 'activeadmin_ckeditor_with_uploads/engine'
 require 'activeadmin_ckeditor_with_uploads/railtie'
 
