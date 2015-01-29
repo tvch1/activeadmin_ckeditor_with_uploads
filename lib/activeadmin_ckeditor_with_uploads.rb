59d5b10e7febb051da594e36d0d6096f0b1b0841
d.krasnov <d.krasnov@1tvch.ru>
2015-01-29 19:00:35 +0300

    minimagick quality method

--- lib/activeadmin_ckeditor_with_uploads.rb
+++ lib/activeadmin_ckeditor_with_uploads.rb
@@ -4,6 +4,8 @@ require 'ckeditor_rails'
 
 require "formtastic/inputs/ckeditor_input"
 
+require 'carrierwave/mini_magick'
+
 require 'activeadmin_ckeditor_with_uploads/uploaders/cke_uploads_file_uploader'
 require 'activeadmin_ckeditor_with_uploads/uploaders/cke_uploads_image_uploader'
 
