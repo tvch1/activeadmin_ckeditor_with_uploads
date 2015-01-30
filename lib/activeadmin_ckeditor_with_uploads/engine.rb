fd90bdbeb2cd37f6a856e11508c244308a85cbf0
d.krasnov <d.krasnov@1tvch.ru>
2015-01-30 11:20:21 +0300

    more experiments with paths

--- lib/activeadmin_ckeditor_with_uploads/engine.rb
+++ lib/activeadmin_ckeditor_with_uploads/engine.rb
@@ -1,7 +1,8 @@
 module ActiveadminCkeditorWithUploads
   class Engine < Rails::Engine
 
-    config.autoload_paths << File.expand_path("../../../app/admin", __FILE__)
+    #config.autoload_paths << File.expand_path("../../../app/admin", __FILE__)
+    paths['app/admin'] << 'app/admin'
 
     initializer "ActiveAdmin precompile hook" do |app|
       app.config.assets.precompile += ['admin/ckeditor.js']
