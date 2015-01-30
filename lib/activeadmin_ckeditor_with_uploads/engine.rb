0bd43c077cc50a9337f5b5e4d5949ea8b4938dce
d.krasnov <d.krasnov@1tvch.ru>
2015-01-30 11:17:44 +0300

    autoload admin resources added to engine

--- lib/activeadmin_ckeditor_with_uploads/engine.rb
+++ lib/activeadmin_ckeditor_with_uploads/engine.rb
@@ -1,6 +1,8 @@
 module ActiveadminCkeditorWithUploads
   class Engine < Rails::Engine
 
+    config.autoload_paths << File.expand_path("../../../app/admin", __FILE__)
+
     initializer "ActiveAdmin precompile hook" do |app|
       app.config.assets.precompile += ['admin/ckeditor.js']
     end
